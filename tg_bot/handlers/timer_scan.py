6486b7afc46628bca2301a366dfbeae16eedd006
jiamid <jiamid@qq.com>
2025-02-17 15:00:55 +0800

    去掉2024以前的

--- tg_bot/handlers/timer_scan.py
+++ tg_bot/handlers/timer_scan.py
@@ -7,6 +7,7 @@
 import asyncio
 from loguru import logger
 import time
+from datetime import datetime
 from commonts.storage_manager import timer_task_storage
 from commonts.util import to_escape_string
 from tg_bot.bot import send_message_to_bot
@@ -32,13 +33,30 @@ async def scan_wallet_transfers(wallet: str, chat_ids: list) -> None:
                     risk_address.append(normal_addr)
             token_transfers = transfers_data.get('token_transfers', [])
             for token_transfer in token_transfers:
-                # this_transfer_ts = token_transfer.get('block_ts', 0)
+                this_transfer_ts = token_transfer.get('block_ts', 0)
+                transfer_time = datetime.fromtimestamp(this_transfer_ts / 1000)
+                if transfer_time.year < 2024:
+                    found_end_flag = True
+                    break
                 from_address = token_transfer.get('from_address', '')
                 if from_address in risk_address:
+                    token_decimal = token_transfer.get('tokenInfo', {}).get('tokenDecimal', 6)
+                    decimal_str = '1' + '0' * token_decimal
+                    quant_str = token_transfer.get('quant', '0')
+                    number = int(int(quant_str) / int(decimal_str))
+                    if number < 1:
+                        logger.info(f'{wallet} found risk {detail_link}')
+                        continue
                     transaction_id = token_transfer.get('transaction_id', '')
                     detail_link = f'https://tronscan.org/#/transaction/{transaction_id}'
-                    text = (f'钱包: *{wallet}*\n 发现异常交易地址:*{from_address}*\n'
-                            f'>交易详情: [{transaction_id}]({to_escape_string(detail_link)})\n')
+                    transfer_time_str = transfer_time.strftime('%Y/%m/%d')
+
+                    text = (f'钱包: *{wallet}*\n'
+                            f'*发现异常交易*\n'
+                            f'时间:*{transfer_time_str}*\n'
+                            f'数量:*{number}*\n'
+                            f'地址:*{from_address}*\n'
+                            f'>交易详情: [{transaction_id}]({to_escape_string(detail_link)})')
                     for chat_id in chat_ids:
                         await send_message_to_bot(chat_id, text, parse_mode='MarkdownV2')
                         logger.info(f'{wallet} found risk {detail_link}')
@@ -48,7 +66,7 @@ async def scan_wallet_transfers(wallet: str, chat_ids: list) -> None:
                 #     break
             if len(token_transfers) < limit:
                 found_end_flag = True
-            else:
+            if not found_end_flag:
                 await asyncio.sleep(1)
                 start += limit
     except Exception as e:
